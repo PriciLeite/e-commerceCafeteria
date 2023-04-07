0df24fdbd402a249de7b010aacc3edb673df9cf0
Priscila Leite <pricila135@gmail.com>
2023-04-06 22:12:47 -0300

    Implementando ILancheRepository e a Casse concreta LancheRepository

---
+++ SalesWebCafeteria/Repository/Interfaces/ILancheRepository.cs
@@ -0,0 +1,9 @@
+﻿using SalesWebCafeteria.Models;
+
+namespace SalesWebCafeteria.Repository.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+    }
+}
---
+++ SalesWebCafeteria/Repository/LancheRepository.cs
@@ -0,0 +1,17 @@
+﻿using SalesWebCafeteria.Models;
+using SalesWebCafeteria.Repository.Interfaces;
+
+namespace SalesWebCafeteria.Repository
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly ILancheRepository _context;
+
+        public LancheRepository(ILancheRepository context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches;
+    }
+}
