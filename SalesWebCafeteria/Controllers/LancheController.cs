6a99e23d03219f3bdf78f47744267a3a716fd407
Priscila Leite <pricila135leite@gmail.com>
2023-04-08 15:14:41 -0300

    Criando o LancheController e Action List + View Lanche/Liet

---
+++ SalesWebCafeteria/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebCafeteria.Repository.Interfaces;
+
+namespace SalesWebCafeteria.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
