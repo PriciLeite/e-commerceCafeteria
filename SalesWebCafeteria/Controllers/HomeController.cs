ec15d600c396de6e5c3413f107e9def276bc65c2
Priscila Leite <pricila135leite@gmail.com>
2023-04-18 19:49:36 -0300

    Criando HomeViewModel - Lista de LanchesPreferidos Criando a Action Index em HomeController Exibindo os LanchesPreferidos na Home Index /

--- SalesWebCafeteria/Controllers/HomeController.cs
+++ SalesWebCafeteria/Controllers/HomeController.cs
@@ -1,18 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebCafeteria.Models;
+using SalesWebCafeteria.Models.ViewModels;
+using SalesWebCafeteria.Repository.Interfaces;
 using System.Diagnostics;
 
 namespace SalesWebCafeteria.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public HomeController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel()
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+            return View(homeViewModel);
         }
-
         
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ SalesWebCafeteria/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace SalesWebCafeteria.Models.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+}
+
