002b991c46ffb14c99fb6e9c3586a89cf39024f6
Priscila Leite <pricila135leite@gmail.com>
2023-04-19 11:58:03 -0300

    TagHelper para envio de e-mail / Criando EmailTagHelper para TagHelpers Importanto o pacote da TagHelper Email para ViewImports / Criando ContatoController / Criando IndexContato

---
+++ SalesWebCafeteria/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebCafeteria.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
