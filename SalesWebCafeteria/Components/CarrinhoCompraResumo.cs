99f654d653fd88ae0926c6cacf4886b4a8baa3f3
Priscila Leite <pricila135leite@gmail.com>
2023-04-18 23:12:20 -0300

    Criando Components / CarrinhoCompraResumo / DefaultView para exibição do carrinho.

---
+++ SalesWebCafeteria/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebCafeteria.Models;
+using SalesWebCafeteria.Models.ViewModels;
+
+namespace SalesWebCafeteria.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            // var Itens = _carrinhoCompra.GetCarrinhoCompraItems();
+
+            var itens = new List<CarrinhoCompraItem>()
+            {
+                new CarrinhoCompraItem(),
+                new CarrinhoCompraItem()
+            };
+
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+
+    }
+}
+
+
+
+
+
+
